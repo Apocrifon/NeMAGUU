e2f41cda419947a955b30d9e94743d0a1754a403
Apocrifon <Astakhov.stepan2003@gmail.com>
2022-10-27 23:21:29 +0500

    fix code style

--- NeMAGUUU/Program.cs
+++ NeMAGUUU/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters;
 using System.Text;
 
 namespace NeMAGUUU
 {
     public class Graph
     {
-        public int[,]  Matrix;
+        public int[,] Matrix;
         int size;
 
         public Graph(int size)
@@ -20,15 +19,16 @@ namespace NeMAGUUU
             Console.WriteLine("info ");// write info
         }
 
-        public void SetConnection( ) //сделать сплит
+        public void SetConnection()
         {
             while (true)
             {
-                int firstNode= int.Parse(Console.ReadLine());
-                if (firstNode == -1)
+                Console.Write("Введите пару вершин: ");
+                var line = Console.ReadLine();
+                if (line == "-1")
                     break;
-                int secondNode = int.Parse(Console.ReadLine());
-                Matrix[firstNode - 1, secondNode - 1] = 1;
+                var numbers = line.Split(' ');
+                Matrix[int.Parse(numbers[0]) - 1, int.Parse(numbers[1]) - 1] = 1;
             }
         }
 
@@ -38,7 +38,7 @@ namespace NeMAGUUU
             {
                 for (int j = 0; j < size; j++)
                 {
-                    Console.Write(Matrix[i,j] + " ");
+                    Console.Write(Matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
@@ -48,84 +48,76 @@ namespace NeMAGUUU
 
     }
 
-    public class Alghoritm
-    {
-        public void FindNodes()
-        {
-
-        }
-    }
-
     class Program
     {
         public static void PrintSets(List<List<int>> sets)
         {
-            foreach (var set in sets)
+            for (int j = 0; j < sets.Count; j++)
             {
-
-                for (int i = 0; i < set.Count; i++)
+                for (int i = 0; i < sets[j].Count; i++)
                 {
-                    Console.Write(set[i] + " + ");
+                    if (i==0)
+                        Console.Write(sets[j][i]);
+                    else
+                        Console.Write(" + " + sets[j][i]);
                 }
-                Console.Write(" * ");
+                if (j != sets.Count-1)
+                    Console.Write(" * ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void AddSetsToList(List<List<int>> sets, List<string> lines, int[,] matrix, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                var list = new List<int>();
+                var line = new StringBuilder();
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        list.Add(j + 1);
+                        line.Append(j + 1);
+                    }
+                }
+                sets.Add(list);
+                lines.Add(line.ToString());
             }
         }
-        
+
         static void Main()
         {
             var sets = new List<List<int>>();
             var lines = new List<string>();
-            var counters = new List<int>();
-            var lineOfNumbers = new StringBuilder();
             Console.Write("Укажите колличество вершин в графе: ");
             var size = int.Parse(Console.ReadLine());
             var matrix = new Graph(size);
             matrix.SetConnection();
             matrix.PrintMatrix();
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < matrix.Matrix.GetLength(0); i++)
+                matrix.Matrix[i, i] = 1;
+            AddSetsToList(sets, lines, matrix.Matrix, size);
+            PrintSets(sets);
+            for (int i = 0; i < lines.Count; i++)
             {
-                var list = new List<int>();
-                list.Add(i + 1);
-                for (int j = 0; j < size; j++)
-                {
-                    if (matrix.Matrix[i, j] == 1)
-                        list.Add(j + 1);
-                }
-                list.Sort();
-                foreach (var item in list)
-                    lineOfNumbers.Append(item);
-                sets.Add(list);
-                lines.Add(lineOfNumbers.ToString());
-                lineOfNumbers.Clear();
-
-            }
-            Program.PrintSets(sets);
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < lines.Count; j++)
                 {
                     if (i == j)
                         continue;
                     if (lines[j].Contains(lines[i]))
                     {
-                        if (!counters.Contains(j))
-                            counters.Add(j);
+                        lines.RemoveAt(j);
+                        sets.RemoveAt(j);
                     }
                 }
             }
-            var newSets = new List<List<int>>();
-            for (int i = 0; i < size; i++)
-            {
-                if (!counters.Contains(i))
-                    newSets.Add(sets[i]);
-            }
-            sets = newSets;
-            //sets = new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 7, 8, 9 } };
+            //sets = new List<List<int>> { new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 4 }, new List<int> { 7, 8, 9 } };
             var setsSize = sets.Count;
             var counter = new int[setsSize - 1];  // чудо счётчик
             var partOfresult = new List<int>();
             var result = new List<List<int>>();
-            while (counter[setsSize-2] < sets[sets.Count-1].Count)
+            while (counter[setsSize - 2] < sets[sets.Count - 1].Count)
             {
 
                 for (int i = 0; i < sets[0].Count; i++)
@@ -138,17 +130,17 @@ namespace NeMAGUUU
                     }
                     result.Add(partOfresult);
                 }
-                    counter[0]++;
-                    for (int m = 0; m < counter.Length-1; m++)
+                counter[0]++;
+                for (int m = 0; m < counter.Length - 1; m++)
+                {
+                    if (counter[m] == sets[m + 1].Count)
                     {
-                        if (counter[m] == sets[m + 1].Count)
-                        {
-                            counter[m] = 0;
-                            counter[m + 1]++;
-                        }
-                    }       
+                        counter[m] = 0;
+                        counter[m + 1]++;
+                    }
+                }
             }
-            
+
             foreach (var set in result)
             {
 
